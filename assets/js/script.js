04181d02bea84d89d82b9040b119a90c7a4ddd89
insertnamehere432 <73672178+insertnamehere432@users.noreply.github.com>
2021-07-28 10:57:02 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 function apex_check() {
   console.log(window.location.href)
+  if (window.location.href.endsWith('index.html')) {
+    window.location.href = window.location.href.replace("index.html","");
+  }
 }
